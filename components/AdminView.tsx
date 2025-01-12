0be14ff6c3ca9ea43ff78bd592e0a4027bf1293e
Carlos Lopez <carloslop0914@gmail.com>
2025-01-11 23:43:39 -0600

    Create header, placeholder for admin view

---
+++ components/AdminView.tsx
@@ -0,0 +1,10 @@
+import { ThemedText } from "./ThemedText";
+import { ThemedView } from "./ThemedView";
+
+export default function AdminView() {
+  return (
+    <ThemedView>
+      <ThemedText type="title">Admin View</ThemedText>
+    </ThemedView>
+  );
+}
